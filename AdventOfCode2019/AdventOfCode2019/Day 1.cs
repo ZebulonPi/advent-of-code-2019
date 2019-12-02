3bee38bdf00123ada5c3059215e012caf219a52e
Jason Greenwood <jgreenw5@its.jnj.com>
2019-12-02 12:45:30 -0500

    Make it pretty for Brad

--- AdventOfCode2019/AdventOfCode2019/Day 1.cs
+++ AdventOfCode2019/AdventOfCode2019/Day 1.cs
@@ -37,31 +37,7 @@ namespace AdventOfCode2019
         The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
         What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.) */
 
-        public static int ComputeFuel(float mass)
-        {
-            int fuel_needed = Convert.ToInt32(Math.Floor(mass / 3.0)) - 2;
-
-            return fuel_needed;
-        }
-
-        public static int ComputeFuelForFuel(float mass)
-        {
-            int fuel_total = 0;
-
-            int fuel_needed = ComputeFuel(mass);
-
-            while (fuel_needed > 0)
-            {
-                fuel_total += fuel_needed;
-                fuel_needed = ComputeFuel(fuel_needed);
-            }
-
-            return fuel_total;
-        }
-
-        public static void Part_1()
-        {
-            String raw_input = @"50669
+        static String raw_input = @"50669
             83199
             108957
             102490
@@ -162,6 +138,31 @@ namespace AdventOfCode2019
             89102
             53428";
 
+
+        public static int ComputeFuel(float mass)
+        {
+            int fuel_needed = Convert.ToInt32(Math.Floor(mass / 3.0)) - 2;
+
+            return fuel_needed;
+        }
+
+        public static int ComputeFuelForFuel(float mass)
+        {
+            int fuel_total = 0;
+
+            int fuel_needed = ComputeFuel(mass);
+
+            while (fuel_needed > 0)
+            {
+                fuel_total += fuel_needed;
+                fuel_needed = ComputeFuel(fuel_needed);
+            }
+
+            return fuel_total;
+        }
+
+        public static void Part_1()
+        {
             String[] numbers = raw_input.Split('\n');
 
             int ans = 0;
@@ -178,107 +179,6 @@ namespace AdventOfCode2019
 
         public static void Part_2()
         {
-            String raw_input = @"50669
-        83199
-        108957
-        102490
-        121216
-        57848
-        76120
-        121042
-        143774
-        106490
-        76671
-        119551
-        109598
-        124949
-        148026
-        119862
-        112854
-        96289
-        73573
-        142714
-        109875
-        126588
-        69748
-        62766
-        104446
-        61766
-        133199
-        118306
-        141410
-        64420
-        129370
-        69444
-        104178
-        109696
-        54654
-        126517
-        138265
-        87100
-        130934
-        138946
-        118078
-        135109
-        137330
-        130913
-        50681
-        53071
-        63070
-        144616
-        64122
-        122603
-        86612
-        144666
-        62572
-        72247
-        79005
-        102223
-        82585
-        54975
-        68539
-        107964
-        148333
-        100269
-        134101
-        115960
-        75866
-        99371
-        56685
-        142199
-        102107
-        84075
-        112733
-        92180
-        131056
-        142803
-        145495
-        70900
-        73129
-        60764
-        77576
-        99160
-        61897
-        78675
-        100890
-        74787
-        131911
-        93106
-        91267
-        142663
-        130649
-        103857
-        81178
-        78896
-        73745
-        84463
-        92926
-        121715
-        74959
-        71911
-        89102
-        53428";
-
             String[] numbers = raw_input.Split('\n');
 
             int ans = 0;
--- AdventOfCode2019/AdventOfCode2019/Program.cs
+++ AdventOfCode2019/AdventOfCode2019/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2019
     {
         static void Main(string[] args)
         {
-            Day_1.Part_2();
+            Day_1.Part_1();
         }
     }
 }
